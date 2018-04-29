615883e15fa7c9cdba93112f743248ecdfa4b4fd
Luc Saffre <luc.saffre@gmail.com>
2018-04-29 18:53:28 +0300

    http://luc.lino-framework.org/blog/2018/0429.html

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
-from atelier.invlib.ns import ns
-ns.setup_from_tasks(globals())
+from atelier.invlib import setup_from_tasks
+ns = setup_from_tasks(globals())
 
 cfg = dict()
 # cfg.update(tolerate_sphinx_warnings=True)
